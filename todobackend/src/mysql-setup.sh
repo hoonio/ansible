50b842f21c4f6900dc09d7b10cd311fb2184ea78
Hoonio <hoonio@hoonio.com>
2016-12-19 18:39:18 +0000

    convert sqlite server to mysql

---
+++ todobackend/src/mysql-setup.sh
@@ -0,0 +1,6 @@
+mysql.server start
+mysql -u root
+
+# CREATE DATABASE todobackend
+# GRANT ALL PRIVILEGES ON *.* TO  'todo'@'localhost' identified by 'password';
+# quit
---
+++ todobackend/src/todobackend/settings/test.py
@@ -0,0 +1,16 @@
+from base import *
+import os
+
+# Database
+# python manage.py test --settings=todobackend.settings.test
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todobackend'),
+        'USER': os.environ.get('MYSQL_USER', 'todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+    }
+}
