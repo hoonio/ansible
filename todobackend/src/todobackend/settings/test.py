d8a1d3ed8b4ade838a1f4271085636ea1f7569c1
Hoonio <hoonio@hoonio.com>
2016-12-19 18:45:35 +0000

    set django nose as test runner

--- todobackend/src/todobackend/settings/test.py
+++ todobackend/src/todobackend/settings/test.py
@@ -4,6 +4,22 @@ import os
 # Database
 # python manage.py test --settings=todobackend.settings.test
 
+INSTALLED_APPS += ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR','.')
+NOSE_ARGS = [
+  '--verbosity=2',
+  '--nologcapture',
+  '--with-coverage',
+  '--cover-package=todo',
+  '--with-spec',
+  '--spec-color',
+  '--with-xunit',
+  '--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
+  '--cover-xml',
+  '--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
